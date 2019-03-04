092ede28497bb9f2138f5ec071f9fc92f594c3bf
mohamed ghomrani <mohamedghomrani@MacBook-Air-de-mohamed.local>
2019-03-04 07:31:55 +0100

    premier test ls

--- Makefile
+++ Makefile
@@ -13,18 +13,25 @@ LIB = libft/libft.a
 all: $(NAME)
 $(NAME): $(LIB) $(OBJ)
 	$(CC) -I includes -o $@ $^ $(LIB) $(FLAGS)
+	@echo "\033[32mBinary \033[1;32m$(NAME)\033[1;0m\033[32m created.\033[0m"
 
 $(LIB):
 	make -C libft
 %.o: %.c 
 	$(CC) -I includes -o $@ -c $< $(FLAGS)
+	@echo "\033[34m\033[1mCompilation of \033[0m\033[36m$(notdir $<)\033[1m\033[34m done.\033[0m"
+
 
 clean:
 	make -C libft clean
 	rm -rf $(OBJ)
+	@echo "\033[31mObjects files \033[1;31m$(OBJS_LIST)\033[1;0m\033[31m removed.\033[0m"
+
 
 fclean: clean
 	rm -rf $(NAME)
 	rm -rf $(LIB)
+	@echo "\033[31mBin \033[1;31m$(NAME)\033[1;0m\033[31m removed.\033[0m"
+
 
 re: fclean all
--- srcs/main.c
+++ srcs/main.c
@@ -1,20 +1,54 @@
 #include "../includes/ft_ls.h"
 
-int main()
+void    ft_exit0()
+{
+    struct dirent *lecture;
+    DIR *rep;
+    t_file *test;   
+
+	test = (t_file *)ft_memalloc(sizeof(t_file)); 
+    test->name = ft_strdup(".");
+    rep = opendir(test->name );
+    while ((lecture = readdir(rep)))
+    {
+        if (ft_strncmp(lecture->d_name, ".", 1) != 0)
+            ft_putendl(lecture->d_name);
+    }
+}
+void ft_exit(char *param)
 {
     struct dirent *lecture;
     DIR *rep;
     t_file *test;
    
 	test = (t_file *)ft_memalloc(sizeof(t_file)); 
-    test->name = ft_strdup(".");
+    test->name = ft_strdup(param);
     rep = opendir(test->name );
   
 	lstat(test->name, &(test->stats));
     while ((lecture = readdir(rep))) 
     {
-        printf("name :%s    type : %d\n", lecture->d_name, lecture->d_type);
+        if (lecture->d_type == 4)
+            printf("d ");
+        else
+        {
+            printf("- ");
+        }
+        
+        printf("%s\n", lecture->d_name);
     }
-    printf("%llu",test->stats.st_ino);
+    //printf("%llu",test->stats.st_ino);
     closedir(rep);
+}
+
+int     main(int argc, char **argv)
+{
+    int i;
+
+    i = argc;
+    if (argc == 1)
+        ft_exit0();
+    else
+        ft_exit(argv[1]);
+    return (0);
 }
